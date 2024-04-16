b058b972eb2261bc0d62abf862ff53c883d0590a
ZhevairovskiBojan <bojanzevairovskijs@gmail.com>
2024-04-16 21:15:27 +0200

    server and modelSchema done

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(" The Office Chat App is up and running!");
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+
+});
